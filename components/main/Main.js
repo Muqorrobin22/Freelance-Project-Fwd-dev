b40aa2fd7fbfe449f925abbb2f0b46d9b1c6f930
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-03-18 16:26:42 +0700

    form in main body done

--- components/main/Main.js
+++ components/main/Main.js
@@ -57,6 +57,109 @@ export default function Main() {
             className={styles.image}
           />
         </div>
+
+        <div className={styles.register}>
+          <h1 className={styles.h1}>Daftar</h1>
+          <form>
+            <div>
+              <label for="nama_lengkap">Nama Lengkap</label>
+              <input
+                type="text"
+                id="nama_lengkap"
+                placeholder="Sesuai Rekening Bank Anda"
+              />
+            </div>
+            <div>
+              <label for="email">Alamat Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Masukkan Email Anda"
+              />
+            </div>
+            <div className={styles.number}>
+              <label for="number">Handphone</label>
+              <div>
+                <select name="number" id="number">
+                  <option value="+62">+62</option>
+                  <option value="+0">+0</option>
+                  <option value="+98">+98</option>
+                  <option value="+88">+88</option>
+                </select>
+                <input type="tel" id="number" placeholder="Ex: 81234456789" />
+              </div>
+            </div>
+            <div>
+              <label for="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                placeholder="6-14 Karakter Huruf dan Angka"
+              />
+            </div>
+            <div>
+              <label for="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="6-14 Karakter"
+              />
+              <ul className={styles.ul}>
+                <li>Minimal 6 kombinasi angka, huruf atau simbol</li>
+                <li>Maksimal 14 kombinasi angka, huruf atau simbol</li>
+              </ul>
+            </div>
+            <div>
+              <label for="confirm_password">Konfirmasi Password</label>
+              <input
+                type="password"
+                id="confirm_password"
+                placeholder="6-14 Karakter"
+              />
+            </div>
+            <div className={styles.knowUs}>
+              <label for="knowUs">
+                Darimana Anda Mengetahui Kami? (opsional)
+              </label>
+              <select name="knowUs" id="knowUs">
+                <option value="choose">-- Pilih Salah Satu --</option>
+                <option value="xxxx">xxx</option>
+                <option value="zzzz">zzzz</option>
+                <option value="aaaa">aaaa</option>
+              </select>
+            </div>
+            <div>
+              <label for="referral_code">Kode Referral (opsional)</label>
+              <input
+                type="text"
+                id="referral_code"
+                placeholder="Kode Referensi"
+              />
+            </div>
+            {/* Code capctha Ini adalah Image karena sifatnya sementara */}
+
+            <div className={styles.capctha}>
+              <Image
+                src={"/images/captcha.png"}
+                alt="Kode Captcha"
+                width={300}
+                height={76}
+              />
+            </div>
+            {/* ------------------------------------------------- */}
+            <div className={styles.agreement}>
+              <p>
+                Dengan menetapkan DAFTAR, Anda menyatakan bahwa benar Anda
+                berusia 18 tahun ke atas dan Anda setuju dengan{" "}
+                <a href="#">Syarat dan Ketentuan</a>
+              </p>
+            </div>
+
+            <button type="submit" className={styles.submit}>
+              DAFTAR
+            </button>
+          </form>
+        </div>
       </section>
     </main>
   );
